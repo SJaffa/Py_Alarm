e7a91238ec01a01c43d72011f5fa15e603e46490
Sai Kasam <saikasam98@gmail.com>
2020-06-28 11:54:03 -0500

    init code

---
+++ alarm.py
@@ -0,0 +1,13 @@
+# to use on linux use mpg123
+# apt install mpg123
+
+import os
+
+def main():
+    # location of the mp3 file
+    file_name = "suprabatham.mp3"
+    # os.system("mpg123 " + file) on linux
+    os.system("afplay " + file_name)
+
+
+main()
