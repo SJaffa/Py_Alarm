a89087a2b43d3767e803be6c51b2e287fa675250
Sai Kasam <saikasam98@gmail.com>
2020-06-28 11:56:21 -0500

    added a comment

--- alarm.py
+++ alarm.py
@@ -7,6 +7,7 @@ def main():
     # location of the mp3 file
     file_name = "suprabatham.mp3"
     # os.system("mpg123 " + file) on linux
+    # on mac afplay stands for audio file play
     os.system("afplay " + file_name)
 
 
